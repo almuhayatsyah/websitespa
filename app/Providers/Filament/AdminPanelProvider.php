27bd875d9606e15d5abaadb413023889efb795aa
Almuhayatsyah <almuhayatsyah2002@gmail.com>
2026-08-14 20:26:19 +0700

    Update Filament admin panel brand name to Spa

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->brandName('Klinik USG 4D')
+            ->brandName('Almuhayatsyah Spa')
             ->login()
             ->colors([
                 'primary' => Color::Cyan,
